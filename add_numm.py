0dbcb24eabd186646e6f72676d7e3cf0757cdd88
Daniel Curtis <dancurtis87@gmail.com>
2013-07-01 16:20:09 -0400

    Added John

--- add_numm.py
+++ add_numm.py
@@ -1,4 +1,4 @@
-#This has been authored by Dan but who knows who will contribute to it in the future.
+#This has been authored by Dan and John  but who knows who will contribute to it in the future.
 def main():
    print ("2+2+2 =", add_three(2,2,2))
    print ("2 + 3 = ",add_two(2,3))
