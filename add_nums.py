27a2a493a7eacdef2504f49b9517a63ef023ae7f
Dominique Poncelet <d.poncelet@ou.edu>
2013-07-01 15:29:53 -0500

    changing exercise

--- add_nums.py
+++ add_nums.py
@@ -1,5 +1,5 @@
-#This has been authorized by Aleksandra but who knows who will contribute in the future.
-
+#This has been authorized by Aleksandra but who knows who will contribute.
+#Dominique is making changes here 07_01_13. 
 def main():
 print ("2+2+2 =", add_three(2, 2, 2))
    print ("2 + 3 = ",add_two(2,3))
