1f0a0f35e269169cb0aaa830659710152aea95e8
Dominique Poncelet <d.poncelet@ou.edu>
2013-07-01 14:37:07 -0500

    Small tweaks to the code

--- add_nums.py
+++ add_nums.py
@@ -1,9 +1,11 @@
 #This has been authorized by Aleksandra but who knows who will contribute in the future.
 def add_two(a,b):
+print ("Hi, I"m just adding two numbers here")
    return a+b
 
 def main():
    print ("2 + 3 = ",add_two(2,3))
+print ("4+4 = ", add_two (4,4))
 
 if __name__ == "__main__":
      main()
